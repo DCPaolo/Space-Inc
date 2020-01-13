36cf1e24e2bf30521420569c039d2ef4f758fe6d
Romain PIOT <romainpiot.pro@gmail.com>
2020-01-14 00:03:33 +0100

    delete tests files

--- app/src/main/java/com/example/spaceinc/MainActivity.kt
+++ app/src/main/java/com/example/spaceinc/MainActivity.kt
@@ -2,17 +2,13 @@ package com.example.spaceinc
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.lifecycle.ViewModelProviders
 
 class MainActivity : AppCompatActivity() {
-
-    lateinit var viewModel: MainViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
     }
+
 }
 
