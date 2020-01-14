bae81a7e961ad90b9a4fceb375721025a3e9b5e3
seb7474 <sebsalerohotmail.fr>
2020-01-14 20:36:14 +0100

    recherche websokets

--- app/src/main/java/com/example/spaceinc/WebSockets.kt
+++ app/src/main/java/com/example/spaceinc/WebSockets.kt
@@ -1,52 +1,62 @@
 package com.example.spaceinc
 
-import android.widget.Button
+import android.content.ContentValues.TAG
+import android.util.Log
 import okhttp3.*
-import kotlin.concurrent.thread
 import okio.ByteString
-import okhttp3.OkHttpClient
-import android.widget.TextView
-
-
 
 
 class WebSockets : WebSocketListener()  {
 
-    private val start: Button? = null
-    private val output: TextView? = null
-    private val client: OkHttpClient? = null
+    fun getCoinPrice(product: String) {
+        val clientCoinPrice = OkHttpClient()
+        val requestCoinPrice: Request = Request.Builder().url("ws://vps769278.ovh.net:8081").build()
+        val webSocketListenerCoinPrice: WebSocketListener = object : WebSocketListener() {
+            override fun onOpen(webSocket: WebSocket, response: Response?) {
+                webSocket.send(
+                    "{\n" +
+                            "    \"type\": \"subscribe\",\n" +
+                            "    \"channels\": [{ \"name\": \"ticker\", \"product_ids\": [\"" + product + "\"] }]\n" +
+                            "}"
+                )
+                Log.e(TAG, "onOpen")
+            }
 
+            override fun onMessage(webSocket: WebSocket, text: String) {
+                Log.e(TAG, "MESSAGE: $text")
+            }
 
+            override fun onMessage(webSocket: WebSocket, bytes: ByteString) {
+                Log.e(TAG, "MESSAGE: " + bytes.hex())
+            }
 
-    override fun onClosed(webSocket: WebSocket, code: Int, reason: String) {
-        super.onClosed(webSocket, code, reason)
+            override fun onClosing(
+                webSocket: WebSocket,
+                code: Int,
+                reason: String
+            ) {
+                webSocket.close(1000, null)
+                webSocket.cancel()
+                Log.e(TAG, "CLOSE: $code $reason")
+            }
+
+            override fun onClosed(
+                webSocket: WebSocket,
+                code: Int,
+                reason: String
+            ) { //TODO: stuff
+            }
+
+            override fun onFailure(
+                webSocket: WebSocket?,
+                t: Throwable?,
+                response: Response?
+            ) { //TODO: stuff
+            }
+        }
+        clientCoinPrice.newWebSocket(requestCoinPrice, webSocketListenerCoinPrice)
+        clientCoinPrice.dispatcher().executorService().shutdown()
     }
 
-    override fun onClosing(webSocket: WebSocket, code: Int, reason: String) {
-        super.onClosing(webSocket, code, reason)
-    }
-
-    override fun onFailure(webSocket: WebSocket, t: Throwable, response: Response?) {
-        super.onFailure(webSocket, t, response)
-    }
-
-    override fun onMessage(webSocket: WebSocket, text: String) {
-        super.onMessage(webSocket, text)
-    }
-
-    override fun onMessage(webSocket: WebSocket, bytes: ByteString) {
-        super.onMessage(webSocket, bytes)
-    }
-
-    override fun onOpen(webSocket: WebSocket, response: Response) {
-        super.onOpen(webSocket, response)
-    }
-
-    private fun start() {
-        //val request = Request.Builder().url("ws://vps769278.ovh.net:8081 ").build()
-        //val listener = EchoWebSocketListener()
-        //val ws = client.newWebSocket(request, listener)
-        //client.dispatcher().executorService().shutdown()
-    }
 }
 
---
+++ app/src/main/java/com/example/spaceinc/waittingRoom/WaittingRoomViewModel.kt
@@ -0,0 +1,11 @@
+package com.example.spaceinc.waittingRoom
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.liveData
+import com.example.spaceinc.model.User
+import com.example.spaceinc.network.UserRepository
+import kotlinx.coroutines.Dispatchers
+
+class WaittingRoomViewModel : ViewModel() {
+
+}
