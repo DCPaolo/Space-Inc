1b05b3923e04accfea14bc022fd402be288fbb0a
Romain PIOT <romainpiot.pro@gmail.com>
2020-01-20 14:55:36 +0100

    reception of rooms info

--- app/src/main/java/com/example/spaceinc/fragments/creationRoom/creationRoomFragment.kt
+++ app/src/main/java/com/example/spaceinc/fragments/creationRoom/creationRoomFragment.kt
@@ -2,31 +2,41 @@ package com.example.spaceinc.fragments.creationRoom
 
 import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
-
 import com.example.spaceinc.R
-import com.example.spaceinc.fragments.login.LoginFragmentDirections
+import com.example.spaceinc.databinding.CreationRoomFragmentBinding
+import com.example.spaceinc.model.Room
+import com.example.spaceinc.model.RoomList
 import kotlinx.android.synthetic.main.creation_room_fragment.*
 
 class creationRoomFragment : Fragment() {
 
-    companion object {
-        fun newInstance() = creationRoomFragment()
-    }
-
     private lateinit var viewModel: CreationRoomViewModel
+    private lateinit var binding: CreationRoomFragmentBinding
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.creation_room_fragment, container, false)
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+
+        // Inflate view and obtain an instance of the binding class
+        binding = DataBindingUtil.inflate(inflater, R.layout.creation_room_fragment, container, false)
+
+        // Get the viewModel
+        viewModel = ViewModelProviders.of(this).get(CreationRoomViewModel::class.java)
+
+        // Set the viewmodel for databinding
+        binding.roomViewModel = viewModel
+
+        showAllRooms()
+
+        return binding.root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
@@ -38,14 +48,34 @@ class creationRoomFragment : Fragment() {
         }
     }
 
+    /** Display all rooms **/
+    private fun showAllRooms() {
+        viewModel.getAllRooms.observe(this, Observer {
+
+            for (room in it) {
+
+
+
+
+            }
+
+
+            val listRoom: List<Room> = it
+            for(item in listRoom){
+                allRooms.append("room : $item \n")
+                Log.i("test", item.toString())
+            }
+        })
+    }
+
     private fun redirectToScore() {
         if (findNavController().currentDestination?.id == R.id.creationRoomFragment) {
 
             Toast.makeText(activity, "Go to scores", Toast.LENGTH_SHORT).show()
             val action = creationRoomFragmentDirections.actionCreationRoomFragmentToScoreFragment()
-
             NavHostFragment.findNavController(this).navigate(action)
         }
     }
+
 
 }
--- app/src/main/java/com/example/spaceinc/network/WebSockets.kt
+++ app/src/main/java/com/example/spaceinc/network/WebSockets.kt
@@ -4,33 +4,30 @@ package com.example.spaceinc.network
 import android.util.Log
 import okhttp3.*
 
-
-
 class WebSockets : WebSocketListener() {
-
 
     var websocket : WebSocket? = null
 
-
     init {
-
         val request =  Request.Builder().url("ws://vps769278.ovh.net:8081/ws").build()
         websocket = OkHttpClient().newWebSocket(request,this)
 
-
     }
-
 
     override fun onMessage(webSocket: WebSocket, text: String) {
         super.onMessage(webSocket, text)
 
-
-        Log.i("toto",text)
+        Log.i("test", text)
     }
 
 
     override fun onOpen(webSocket: WebSocket, response: Response) {
 
+    }
+
+    private fun joinRoom(roomName:String, userId:Int) {
+        val request =  Request.Builder().url("ws://vps769278.ovh.net:8081/ws/join/$roomName/$userId").build()
+        websocket = OkHttpClient().newWebSocket(request,this)
     }
 
 
