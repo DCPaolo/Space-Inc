8fe660c71a93ffef1156cfeeb19ec945062db689
seb7474 <sebsalerohotmail.fr>
2020-01-20 12:02:04 +0100

    WebSocket is working

--- app/src/main/java/com/example/spaceinc/MainActivity.kt
+++ app/src/main/java/com/example/spaceinc/MainActivity.kt
@@ -2,12 +2,15 @@ package com.example.spaceinc
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.example.spaceinc.network.WebSockets
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        WebSockets()
 
 
     }
---
+++ app/src/main/java/com/example/spaceinc/network/WebSockets.kt
@@ -0,0 +1,39 @@
+package com.example.spaceinc.network
+
+
+import android.util.Log
+import okhttp3.*
+
+
+
+class WebSockets : WebSocketListener() {
+
+
+    var websocket : WebSocket? = null
+
+
+    init {
+
+        val request =  Request.Builder().url("ws://vps769278.ovh.net:8081/ws").build()
+        websocket = OkHttpClient().newWebSocket(request,this)
+
+
+    }
+
+
+    override fun onMessage(webSocket: WebSocket, text: String) {
+        super.onMessage(webSocket, text)
+
+
+        Log.i("toto",text)
+    }
+
+
+    override fun onOpen(webSocket: WebSocket, response: Response) {
+
+    }
+
+
+
+
+}
